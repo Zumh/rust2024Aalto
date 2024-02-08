2548e8ac58f0e2d469a01712448668309300666a
Zumh <17211423+Zumh@users.noreply.github.com>
2024-02-07 19:55:35 -0500

    Create arrayIndexing.rs

---
+++ DataStructures/arrayIndexing.rs
@@ -0,0 +1,13 @@
+// Implement the ith_element function to return the ith element of the array.
+fn ith_element(array: [u16; 3], i: usize)->u16{
+
+    array[i]
+    
+    
+}
+fn main() {
+    let a = [1, 5, 9];
+    let i = 1;
+    println!("{}", ith_element(a, i));
+}
+

760511a46846c6f4c8e66e3b6722677e9dac1657
Zumh <17211423+Zumh@users.noreply.github.com>
2024-03-07 11:47:41 -0500

    update floating types

---
+++ SyntaxAndBasicConcepts/types/floatingTypes.rs
@@ -0,0 +1,7 @@
+fn main() {
+    // string types
+    let s = "I hAte tHis sTyLe Of WrItInG";
+
+    println!("{}",s.to_lowercase());
+    //println!("{s.to_lowercase()}"); // doesn't work because of s.to... need to compile the string literal first
+}

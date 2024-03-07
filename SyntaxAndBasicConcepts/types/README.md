b222431e6cf870b2d3485a45dc158b422f4ca345
Zumh <17211423+Zumh@users.noreply.github.com>
2024-03-07 11:53:58 -0500

    string types and floating types

---
+++ SyntaxAndBasicConcepts/types/README.md
@@ -0,0 +1,1 @@
+https://doc.rust-lang.org/reference/types.html
--- SyntaxAndBasicConcepts/types/stringTypes.rs
+++ SyntaxAndBasicConcepts/types/stringTypes.rs
@@ -4,4 +4,10 @@ fn main() {
 
     println!("{}",s.to_lowercase());
     //println!("{s.to_lowercase()}"); // doesn't work because of s.to... need to compile the string literal first
+
+    // replace every t with j
+    println!("{}", "testing".replace("t", "j"));
+
+    // replace first n matches
+    println!("{}", "testing".replacen("t", "j", 1)); 
 }

f8e93581996cd2864f2fef95478265d7051d3a07
Zumh <17211423+Zumh@users.noreply.github.com>
2024-03-14 20:29:30 -0400

    fact matching pattern is added

---
+++ ConditionAndErrorHandling/matching_patterns/fact.rs
@@ -0,0 +1,9 @@
+// This for match expression that match the pattern.
+fn main() {
+    let fact = true;
+    match fact {
+    true => println!("fact"), 
+    false => println!("not fact"), 
+    
+    }
+}

88baa982656b2043b4b1061919c609a2f8ee50c5
Zumh <17211423+Zumh@users.noreply.github.com>
2024-03-15 14:01:28 -0400

    null update

--- ConditionAndErrorHandling/optional_values/null_to_default.rs
+++ ConditionAndErrorHandling/optional_values/null_to_default.rs
@@ -4,4 +4,8 @@ fn main(){
     println!("{}", maybe_a_value.unwrap_or("default value"));
     maybe_a_value = None;
     println!("{}", maybe_a_value.unwrap_or("null"));
+
+    // Another useful method is expect, which causes panic like unwrap, but also allows us to specify a custom error message to be shown after the panic.
+    let bob: Option<&str> = None;
+    println!("{}", bob.expect("no value"));
 }

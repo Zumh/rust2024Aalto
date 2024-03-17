a8751266fcec405affc6aa050d07c1547d54c64b
Zumh <17211423+Zumh@users.noreply.github.com>
2024-03-17 16:59:49 -0400

    vectors update

---
+++ DataStructures/vectors/adding_and_removing_vec.rs
@@ -0,0 +1,33 @@
+// push and append method will be explore here
+// get, contains, join, sort work for vectors.
+
+fn main() {
+    println!("Adding vectors!");
+    let mut v: Vec<i32> = Vec::new(); 
+    println!("{}", v.len());
+
+    v.push(1);
+    v.push(2);
+    println!("{}", v.len()); // 2
+    println!("{v:?}"); // [1, 2]
+    
+
+    let mut v2 = vec![1, 2, 3];
+
+    v2.append(&mut v);
+    println!("{}", v2.len()); // 3
+    println!("{v:?}");
+    println!("{v2:?}"); // [1, 2, 3]
+    println!("\n\nremoving"); 
+    removing();
+
+}
+
+fn removing() {
+    let mut v = vec![1, 2, 3, 4, 5];
+    println!("{v:?}"); // [1, 2, 3, 4, 5]
+    v.remove(0);
+    println!("{v:?}"); // [2, 3, 4, 5]
+    let popped = v.pop();
+    println!("pooped: {popped:?}, remaning: {v:?}"); // pooped: 5 {v:?}"); // [2, 3, 4]
+}

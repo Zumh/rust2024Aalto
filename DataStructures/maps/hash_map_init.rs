dcb89e1ad1b8d0ea7b3f0c8b289ef6c616aa7c0e
Zumh <17211423+Zumh@users.noreply.github.com>
2024-03-17 19:49:58 -0400

    we add how to modify hashmap

--- DataStructures/maps/hash_map_init.rs
+++ DataStructures/maps/hash_map_init.rs
@@ -1,3 +1,4 @@
+
 use std::collections::HashMap;
 
 fn main(){
@@ -7,6 +8,29 @@ fn main(){
     country_pollutions();
     println!("\n\n remove entry");
     remove_entry();
+
+    println!("\n\n Modifying hashmap");
+    modifying();
+}
+
+fn modifying(){
+    // get_mut which get a mutalb erference to the value inside an Option
+    let mut food_prices = HashMap::from([("beetroot", 1.2), ("cabbage", 1.1)]);
+    println!("before inflation {food_prices:#?}");
+
+    println!("some inflation happens...");
+
+    // unwrap when we aren't afraid of panicking
+    let beetroot_price: &mut f32 = food_prices.get_mut("beetroot").unwrap();
+    *beetroot_price *= 1.2;
+
+    // let us not forget `if let`
+    if let Some(cabbage_price) = food_prices.get_mut("cabbage") {
+        *cabbage_price *= 1.2;
+    }
+
+    println!("after inflation {food_prices:#?}");
+
 }
 
 
@@ -25,8 +49,14 @@ fn remove_entry(){
         ("Brazil", 7),
     ]);
     println!("{:#?}", country_pollutions);
-    country_pollutions.remove_entry("US");
-    println!("\n remove {}", "US");
+    // simple remove 
+    country_pollutions.remove("UK");
+    println!("\n remove {}", "UK");
+    println!("{:#?}", country_pollutions);
+    
+    // return value
+    let removed = country_pollutions.remove_entry("US");
+    println!("\n remove {}", removed.unwrap().0);
     println!("{:#?}", country_pollutions);
 }
 
